0a90657faf5f6327b3546e33f8aee3d224ff3ce1
Пронин Егор <mrzillagold@gmail.com>
2021-04-11 05:11:23 +1000

    Remove router package

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { SplitCol, SplitLayout, View, Root, ViewWidth, useAdaptivity } from "@vkontakte/vkui";
-import { useStructure, useSwipeBack } from "@unexp/router";
 
 import { Connection, Error } from "./panels";
 
@@ -12,15 +11,6 @@ export function Layout() {
     const path = window.location.pathname.replaceAll("/", "");
 
     const { viewWidth } = useAdaptivity();
-    const { popout, view, panel } = useStructure({
-        view: "home",
-        panel: connections.includes(path) ?
-            "connection" :
-            panels.includes(path) ?
-                path
-                :
-                "404"
-    });
 
     const isDesktop = viewWidth > ViewWidth.MOBILE;
     const width = isDesktop ? "560px" : "100%";
@@ -32,13 +22,16 @@ export function Layout() {
                       width={width}
                       maxWidth={width}
             >
-                <Root modal={null}
-                      activeView={view}
-                      popout={popout}
-                >
+                <Root activeView="home">
                     <View id="home"
-                          activePanel={panel}
-                          {...useSwipeBack()}
+                          activePanel={
+                              connections.includes(path) ?
+                                  "connection" :
+                                  panels.includes(path) ?
+                                      path
+                                      :
+                                      "404"
+                          }
                     >
                         <Connection id="connection"/>
                         <Error id="404"/>
