e037510b6c5795020fa36f59cfd99914a641b46e
Пронин Егор <mrzillagold@gmail.com>
2021-04-11 05:26:58 +1000

    Remove router package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { AdaptivityProvider, AppRoot } from "@vkontakte/vkui";
-import { Router } from "@unexp/router";
 
 import { AppearanceProvider } from "./components";
 
@@ -10,14 +9,12 @@ import "./App.css";
 
 export function App() {
     return (
-        <Router>
-            <AdaptivityProvider>
-                <AppearanceProvider>
-                    <AppRoot>
-                        <Layout/>
-                    </AppRoot>
-                </AppearanceProvider>
-            </AdaptivityProvider>
-        </Router>
+        <AdaptivityProvider>
+            <AppearanceProvider>
+                <AppRoot>
+                    <Layout/>
+                </AppRoot>
+            </AppearanceProvider>
+        </AdaptivityProvider>
     );
 }
